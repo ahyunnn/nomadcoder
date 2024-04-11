9aa9f8c6fab2bcd47ace9972b730df8928ee67d2
ahyunnn <ahyun3089@gmail.com>
2024-04-11 17:00:07 +0700

    240411 - nomadcoder - reactjs - inputs and state

--- movie-react-app/src/App.js
+++ movie-react-app/src/App.js
@@ -1,35 +1,14 @@
-import { useState } from "react";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (todo === "") {
-      return;
-    }
-    setTodos((currentArray) => [todo, ...currentArray]);
-    setTodo("");
-  };
-  console.log(todos);
+
   return (
-    <div>
-      <h1>My TODOS ({todos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={todo}
-          type="text"
-          placeholder="write your to do..."
-        />
-        <button>Add to do</button>
-      </form>
-      <hr />
-      {todos.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
-    </div>
+   <div>
+    <h1 >Super Converter</h1>
+    <label for="minutes">Minutes</label>
+    <input id="minutes" placeholder="Minutes" type="number"/>
+    <label for="hours">Hours</label>
+    <input id="hours"placeholder="Hours"type="number"/>
+   </div>
   );
 }
 
