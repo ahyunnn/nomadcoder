1dd9605f91eed0c0caff86b12fb7fcff5d457b12
ahyunnn <ahyun3089@gmail.com>
2024-04-17 10:38:16 +0700

    240417 - nomadcoder - create to do list

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onChange = (event) => {
+    setTodo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodos((currentArray) => [todo, ...currentArray]);
+    setTodo("");
+  };
+  console.log(todos);
+  console.log(todos.map((item, index) => <li key={index}>{item}</li>)); // react의 element
   return (
     <div>
-      <input type="text" />
+      <h3>My To Dos ({todos.length})</h3>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={todo} type="text" placeholder="Write your to do" />
+        <button>Add to do</button>
+      </form>
+      <hr />
+      <ul>
+        {todos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
