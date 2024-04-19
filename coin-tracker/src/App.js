b3dd2f59793acce4ba4e503f55cf02a69cf6d4f1
ahyunnn <ahyun3089@gmail.com>
2024-04-19 10:34:56 +0700

    240419 - nomadcoder - complete coin-tracker

--- coin-tracker/src/App.js
+++ coin-tracker/src/App.js
@@ -2,15 +2,32 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json)
-      .then((json) => console.log(json));
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+        console.log(json);
+      });
   }, []);
+
   return (
     <div>
       <h3>Coin tracker</h3>
-      {loading ? <p>loading...</p> : null}
+      {loading ? (
+        <p>loading...</p>
+      ) : (
+        <select>
+          {coins.map((item) => (
+            <option>
+              {item.name} {item.symbol} ${item.quotes.USD.price.toFixed(2)} / How many coins can I get? with $20 :{" "}
+              {Math.round(20 / item.quotes.USD.price.toFixed(2))}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
