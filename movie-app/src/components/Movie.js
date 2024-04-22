448138f108c4b330b211476ee035b291fcc0db6b
ahyunnn <ahyun3089@gmail.com>
2024-04-22 14:11:05 +0700

    240422 - movieApp - complete detail page

--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -2,18 +2,19 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
-function Movie({ key, coverImg, title, summary, genres }) {
+function Movie({ id, key, coverImg, title, summary, genres }) {
   return (
     <div key={key}>
       <img src={coverImg} alt={title} />
       <h3>
-        <Link to="/movie">{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h3>
       <p>{summary}</p>
       <ul>
--- movie-app/src/routes/Detail.js
+++ movie-app/src/routes/Detail.js
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  // 주소에서 영화의 id를 가져오기
+  const { id } = useParams();
+
+  // 주소 정보 관리
+  const [movie, setMovie] = useState([]);
+
+  // 로딩 관리
+  const [loading, setLoading] = useState(true);
+  const getMovieDetail = async () => {
+    const json = await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
+    const data = json.data.movie;
+    setMovie(data);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovieDetail();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h3>loading...</h3>
+      ) : (
+        <div>
+          <h3>{movie.title}</h3>
+          <img src={movie.medium_cover_image} alt="" />
+          <p>{movie.description_full}</p>
+          <p>rating : {movie.rating}</p>
+          <p>year : {movie.year}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
