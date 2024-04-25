294b7c19475d4ca880a74d97eea77827c8bc1f69
ahyunnn <ahyun3089@gmail.com>
2024-04-25 16:57:24 +0700

    240425 - reactMaster - fix styled-components code

--- react-master/src/App.js
+++ react-master/src/App.js
@@ -5,23 +5,21 @@ const Father = styled.div`
 `;
 
 const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
+  background-color: ${(props) => props.$bgColor};
   width: 100px;
   height: 100px;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 function App() {
   return (
     <Father>
-      <Box bgColor="teal" />
-      <Box bgColor="tomato" />
+      <Box $bgColor="teal" />
+      <Circle $bgColor="tomato" />
     </Father>
-
-    // 그 전에 사용하던 방식
-    // <div style={{ display: "flex" }}>
-    //   <div style={{ backgroundColor: "teal", width: 100, height: 100 }}></div>
-    //   <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
-    // </div>
   );
 }
 
